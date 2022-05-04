6bbd6f839b1c908e5e97dfe3d8635df32b966ca7
Matias Su Lavik <matias.lavik@varjo.com>
2022-05-04 10:52:41 +0200

    Entities sample: Use Burst compiler.

--- 4 - Unity ECS/UnityBallsEntities/Assets/Scripts/BallMovementSystem.cs
+++ 4 - Unity ECS/UnityBallsEntities/Assets/Scripts/BallMovementSystem.cs
@@ -2,18 +2,26 @@
 using Unity.Transforms;
 using Unity.Mathematics;
 
+public struct GravityWellComponent_GO_ECS : IComponentData
+{
+    public float Strength;
+    public float Radius;
+    // Include position of gravity well so all data accessible in one location
+    public float3 Position;
+}
+
 /// <summary>
 /// Component system for moving all the balls.
 /// The system will automatically run (no need to reference it anywhere).
 /// Boxes move with a constant speed in a random direction inside a box (AABB). They will bounce off the walls.
 /// </summary>
-public class BallMovementSystem : ComponentSystem
+public partial class BallMovementSystem : SystemBase
 {
     /// <summary>
     /// Get intersection with the axis alligned bounding box.
     /// </summary>
     /// <returns>tNear, tFar</returns>
-    private float2 intersectAABB(float3 rayOrigin, float3 rayDir, float3 boxMin, float3 boxMax)
+    private static float2 intersectAABB(float3 rayOrigin, float3 rayDir, float3 boxMin, float3 boxMax)
     {
         float3 tMin = (boxMin - rayOrigin) / rayDir;
         float3 tMax = (boxMax - rayOrigin) / rayDir;
@@ -26,14 +34,19 @@ public class BallMovementSystem : ComponentSystem
 
     protected override void OnUpdate()
     {
-        Entities.WithAll<BallData>().ForEach((ref Translation trans, ref BallData boidData) =>
+        // Store these variables in local variables, since WithBurst().ForEach() can't capture "this".
+        float boxExtents = SimulationSettings.BoxExtents;
+        float deltaTime = Time.DeltaTime;
+
+        // Iterate over all entities that have Translation and BallData, with a Burst-compiled lambda (all code below will be compiled with Burst).
+        Entities.WithName("BallMovement").WithBurst().ForEach((ref Translation trans, ref BallData boidData) =>
         {
             // Box extents
-            const float boxMin = -SimulationSettings.BoxExtents / 2;
-            const float boxMax = SimulationSettings.BoxExtents / 2;
+            float boxMin = -boxExtents / 2;
+            float boxMax = boxExtents / 2;
 
             // Distance to travel this frame
-            float currStep = boidData.speed * Time.DeltaTime;
+            float currStep = boidData.speed * deltaTime;
 
             // Find intersection with axis aligned bounding box
             float3 rayOrigin = trans.Value;
@@ -50,11 +63,11 @@ public class BallMovementSystem : ComponentSystem
             // This is a vectorized version of:
             //   if(abs(newPos.x) >= boxMax)
             //      direction.x *= -1.0f
-            //   ... for all 3 vector components!
+            //   ... for x, y and z.
             boidData.direction *= (math.step(new float3(boxMax, boxMax, boxMax), math.abs(newPos)) * -2 + 1);
 
             // Set new position
             trans.Value = newPos;
-        });
+        }).Run();
     }
 }
